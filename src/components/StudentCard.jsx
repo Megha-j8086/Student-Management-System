8c2d8d1ee1eb26055c15af2e81219f782dda1e3b
Megha-j8086 <www.meghaj80@gmail.com>
2026-03-11 12:22:51 +0530

    Update StudentCard.jsx

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -10,22 +10,24 @@ const StudentCard = (props) => {
           <h5 className="card-title">{props.name}</h5>
           <p className="card-text">Course: {props.course}</p>
           <p className="card-text">Batch: {props.batch}</p>
+          <p className="card-text">Email: {props.email}</p>
+          <p className="card-text">Phone No: {props.phoneno}</p>
 
           <button
-            className="btn btn-primary btn-sm me-2"
+            className="btn btn-primary btn-sm me-2 p-2"
             onClick={props.onView}
           >
             View
           </button>
           <button
-            className="btn btn-primary btn-sm me-2"
+            className="btn btn-primary btn-sm me-2 p-2"
             onClick={props.onEdit}
           >
             Edit
           </button>
 
           <button
-            className="btn btn-danger btn-sm"
+            className="btn btn-danger btn-sm p-2"
             onClick={props.onDelete}
           >
             Delete
